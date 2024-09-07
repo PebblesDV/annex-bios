3938cc0ca72afffd195ba3ddc82058a7d9fc0d0b
PebblesDV <pebblesdevries@gmail.com>
2024-09-07 19:29:48 +0200

    small fixes

--- filters.php
+++ filters.php
@@ -18,16 +18,16 @@
     <div class="filters">
         <img class="filter-img" src="assets/images/icons/sliders.svg" alt="filters">
         <div class="checkbox">
-            <input type="checkbox" class="checkbox-btn" name="films" value="Films">
-            <p class="checkbox-text">FILMS</p>
+            <input type="checkbox" id="films-btn" class="checkbox-btn" name="films" value="Films">
+            <label for="films-btn" class="checkbox-text">FILMS</label>
         </div>
         <div class="checkbox">
-            <input type="checkbox" class="checkbox-btn" name="deze week" value="Deze week">
-            <p class="checkbox-text">DEZE WEEK</p>
+            <input type="checkbox" id="week-btn" class="checkbox-btn" name="deze week" value="Deze week">
+            <label for="week-btn" class="checkbox-text">DEZE WEEK</label>
         </div>
         <div class="checkbox">
-            <input type="checkbox" class="checkbox-btn" name="vandaag" value="Vandaag">
-            <p class="checkbox-text">VANDAAG</p>
+            <input type="checkbox" id="today-btn" class="checkbox-btn" name="vandaag" value="Vandaag">
+            <label for="today-btn" class="checkbox-text">VANDAAG</label>
         </div>
         <div class="checkbox">
             <p class="checkbox-text">CATEGORIE</p>

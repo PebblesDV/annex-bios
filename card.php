7ea6e07465aca521012c7a34a570a74aa9f8d10f
PebblesDV <pebblesdevries@gmail.com>
2024-09-17 12:12:45 +0200

    responsive voor groot scherm

--- card.php
+++ card.php
@@ -2,49 +2,51 @@
 im thinking about creating a true/false variable that changes if you load in index.php or film-agenda.php
 If its on film agenda i just let it load normally, if it is on index.php
 i can use a while loop for like $i = 1 while($i < 6) {do the things but also a $i++;}-->
-    <div class="all-cards">
-        <!-- start for each loop here for singular object so it loads in through foreach loop-->
+<div class="all-cards">
+    <!-- start for each loop here for singular object so it loads in through foreach loop-->
 
     <?php
 
-        $i = 0;
-        // var_dump($playingMovieData);
+    $i = 0;
+    // var_dump($playingMovieData);
 
-        if (1 == 1) {
-            foreach($playingMovieData['data'] as $playingMovieData){
-    //     echo $playingMovieData['movie_id'];
-        foreach($movieData["data"] as $data){
-          if($lessMovies && $i >=6){
-              break;
-          }         
+    if (1 == 1) {
+        foreach ($playingMovieData['data'] as $playingMovieData) {
+            //     echo $playingMovieData['movie_id'];
+            foreach ($movieData["data"] as $data) {
+                if ($lessMovies && $i >= 6) {
+                    break;
+                }
 
-          //put $i++ here if u want it to break or sum idk
+                //put $i++ here if u want it to break or sum idk
 
-           if($data['api_id'] == $playingMovieData['movie_id']){
-            //load in movies
-            
-            $i++;
+                if ($data['api_id'] == $playingMovieData['movie_id']) {
+                    //load in movies
+
+                    $i++;
 
 
-        ?>
-        <div class="card">
-            <img class="card-img" src="<?=$data['image']?>" alt="movie">
-            <div class="card-info">
-                <h2 class="text"><?=$data['title']?></h2>
-                <p class="text">Release: <?=$data['release_date']?></p>
-                <p class="movie-info text"><?=$data['description']?></p>
-                <a href="film-detail.php?id=<?=$data['imdb_id']?>" class="card-btn">MEER INFO & TICKETS</a>
-            </div>
-        </div>   
-        <?php    
-                     }
-                 } 
+    ?>
+                    <div class="card">
+                        <img class="card-img" src="<?= $data['image'] ?>" alt="movie">
+                        <div class="info-and-btn">
+                            <div class="card-info">
+                                <h2 class="text"><?= $data['title'] ?></h2>
+                                <p class="text">Release: <?= $data['release_date'] ?></p>
+                                <p class="movie-info text"><?= $data['description'] ?></p>
+                            </div>
+                            <a href="film-detail.php?id=<?= $data['imdb_id'] ?>" class="card-btn">MEER INFO & TICKETS</a>
+                        </div>
+                    </div>
+    <?php
+                }
+            }
         }
     } else {
         echo 'error, no "data" found';
     }
-      ?>
-    </div>
+    ?>
+</div>
 
 </body>
 

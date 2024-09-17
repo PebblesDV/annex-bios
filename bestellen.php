cdad3191660e596243395bfa33af58195e443d9e
PebblesDV <pebblesdevries@gmail.com>
2024-09-17 12:24:35 +0200

    telefoon reponsive fix

--- bestellen.php
+++ bestellen.php
@@ -35,7 +35,7 @@ include_once('APIconnect.php');
             </div>
             <?php
 
-//NEGGEER DIT STUKIE IS GWN ALVAST VOOR DATA DATA IN LADEN ENZO 
+            //NEGGEER DIT STUKIE IS GWN ALVAST VOOR DATA DATA IN LADEN ENZO 
             //checking if it got send by the header or not
             // if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST != null) {
 
@@ -45,28 +45,28 @@ include_once('APIconnect.php');
             //         foreach ($playingMovieData['data'] as $data2) {
             //             foreach ($movieData['data'] as $data) {
             //                 if ($data['imdb_id'] == $selectedMovie) {
-                                //do the things to load it in
+            //do the things to load it in
             ?>
 
-                                <div class="order-filters">
-                                    <p class="filter"><?= $data['title'] ?></p>
+            <div class="order-filters">
+                <p class="filter"><?= $data['title'] ?></p>
 
-                                    <form action="">
-                                        <select class="filter" name="date" id="date">
-                                            <option value="" disabled selected hidden>DATUM</option>
-                                            <option value="08-06">8 juni</option>
-                                            <option value="15-09">15 september</option>
-                                        </select>
-                                    </form>
+                <form action="">
+                    <select class="filter" name="date" id="date">
+                        <option value="" disabled selected hidden>Datum</option>
+                        <option value="08-06">8 juni</option>
+                        <option value="15-09">15 september</option>
+                    </select>
+                </form>
 
-                                    <form action="">
-                                        <select class="filter" name="time" id="time">
-                                            <option value="" disabled selected hidden>TIJDSTIP</option>
-                                            <option value="10:00">10:00</option>
-                                            <option value="12:15">12:15</option>
-                                        </select>
-                                    </form>
-                                </div>
+                <form action="">
+                    <select class="filter" name="time" id="time">
+                        <option value="" disabled selected hidden>Tijdstip</option>
+                        <option value="10:00">10:00</option>
+                        <option value="12:15">12:15</option>
+                    </select>
+                </form>
+            </div>
         </div>
 
         <div class="order-and-info">
@@ -222,49 +222,49 @@ include_once('APIconnect.php');
             <a href="" class="pay-btn bold">AFREKENEN</a>
         </div>
 
-    <?php
-            //                 }
-            //             }
-            //         }
-            //     } else {
-            //         //give error
-            //         echo 'No given id';
-            //     }
-            // } else {
-            //     //load in the stuff w the index.php way
-                $bwap = false;
-            if ($bwap == true){
+        <?php
+        //                 }
+        //             }
+        //         }
+        //     } else {
+        //         //give error
+        //         echo 'No given id';
+        //     }
+        // } else {
+        //     //load in the stuff w the index.php way
+        $bwap = false;
+        if ($bwap == true) {
             //     if (!empty($_GET['id'])) {
 
             //         $id = $_GET['id'];
             //         foreach ($playingMovieData['data'] as $data2) {
             //             foreach ($movieData['data'] as $data) {
             //                 if ($data['imdb_id'] == $id) {
-    ?>
+        ?>
 
-        <div class="order-filters">
-            <p class="filter"><?= $data['title'] ?></p>
+            <div class="order-filters">
+                <p class="filter"><?= $data['title'] ?></p>
 
-            <form action="">
-                <select class="filter" name="date" id="date">
-                    <option value="" disabled selected hidden>DATUM</option>
-                    <?php
-                                foreach ($data2['play_time'] as $day) { ?>
-                        <option value="08-06">8 juni</option>
-                    <?php
-                                }
-                    ?>
-                </select>
-            </form>
+                <form action="">
+                    <select class="filter" name="date" id="date">
+                        <option value="" disabled selected hidden>DATUM</option>
+                        <?php
+                        foreach ($data2['play_time'] as $day) { ?>
+                            <option value="08-06">8 juni</option>
+                        <?php
+                        }
+                        ?>
+                    </select>
+                </form>
 
-            <form action="">
-                <select class="filter" name="time" id="time">
-                    <option value="" disabled selected hidden>TIJDSTIP</option>
-                    <option value="10:00">10:00</option>
-                    <option value="12:15">12:15</option>
-                </select>
-            </form>
-        </div>
+                <form action="">
+                    <select class="filter" name="time" id="time">
+                        <option value="" disabled selected hidden>TIJDSTIP</option>
+                        <option value="10:00">10:00</option>
+                        <option value="12:15">12:15</option>
+                    </select>
+                </form>
+            </div>
     </div>
 
     <div class="order-and-info">
@@ -424,7 +424,7 @@ include_once('APIconnect.php');
             //         }
             //     }
             // }
-                            }
+        }
 ?>
 </div>
 
